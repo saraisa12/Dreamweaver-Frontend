d86a2a39d1f910c84aaa250edc869c0110ca74f1
saraisa12 <saraisa.024@gmail.com>
2024-10-15 14:48:32 +0300

    not done

--- src/components/CardEvent.jsx
+++ src/components/CardEvent.jsx
@@ -2,6 +2,14 @@ import { Link } from "react-router-dom"
 const CardEvent = ({ event, handleDelete }) => {
   return (
     <div>
+      {event.image && (
+        <img
+          src={event.image}
+          alt={event.name}
+          style={{ width: "100%", height: "auto", borderRadius: "8px" }}
+        />
+      )}
+
       <h2>{event.name}</h2>
 
       <Link to={`/event/details/${event._id}`}>View Details</Link>
