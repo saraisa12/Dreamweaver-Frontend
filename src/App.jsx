23c20b1794c537a8cc39e7d072f1b9b661df15ba
saraisa12 <saraisa.024@gmail.com>
2024-10-15 12:13:42 +0300

    done with reserve ticket

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom"
 import AddEvent from "./components/AddEvent"
 import ListEvents from "./components/ListEvents"
 import DetailsEvent from "./components/DetailsEvent"
+import ReserveEvent from "./components/ReserveEvent"
 
 const App = () => {
   return (
@@ -12,7 +13,9 @@ const App = () => {
       <main>
         <Routes>
           <Route path="/event/index" element={<ListEvents />} />{" "}
+          <Route path="/event/add" element={<AddEvent />} />{" "}
           <Route path="/event/details/:id" element={<DetailsEvent />} />{" "}
+          <Route path="/event/reserve/:id" element={<ReserveEvent />} />{" "}
           {/* Route for Event Details */}
         </Routes>
       </main>
